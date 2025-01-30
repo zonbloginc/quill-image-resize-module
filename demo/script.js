b9f1213c6adc3c018a8734e2c22b88689aadb098
mgreminger <michael.greminger@gmail.com>
2025-01-30 10:17:52 -0600

    fix: update to work with Quill 2

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,9 @@
+import 'quill/dist/quill.snow.css';
+import Quill from 'quill';
+import ImageResize from '../lib/ImageResize';
+
+Quill.register('modules/imageResize', ImageResize);
+
 var quill = new Quill('#editor', {
 	theme: 'snow',
 	modules: {
